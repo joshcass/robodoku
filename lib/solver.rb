09934733954221326c9555526da7c18fc985553b
Josh Cass <joshuacass@gmail.com>
2015-05-05 21:07:53 -0600

    solves adult easy puzzles and some refactoring

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,9 +1,63 @@
-require_relative 'board'
+require_relative 'spot'
+require_relative 'candidate_removers'
 
 class Solver
   def self.solve(board_chars)
     board = Board.new board_chars
     board.remove_all_candidates until board.all_locked?
     board.spots.map &:value
+  end
+end
+
+class Board
+  attr_reader :rows, :columns, :squares, :spots
+
+  def initialize(board_chars)
+    @spots   = board_chars.map { |value| Spot.new value }
+    @rows    = spots.each_slice(9).map { |slice| Row.new slice }
+    @columns = @rows.transpose.map { |spots| Column.new spots }
+    @squares = spots.each_slice(9)
+                    .each_slice(3)
+                    .flat_map {|row| row.transpose.flatten}
+                    .each_slice(9)
+                    .map {|sqr_spots| Square.new(sqr_spots)}
+  end
+
+  def remove_all_candidates
+    rows.each {|row| row.remove_candidates}
+    columns.each {|column| column.remove_candidates}
+    squares.each {|square| square.remove_candidates}
+  end
+
+  def all_locked?
+    spots.all? {|spot| spot.locked?}
+  end
+end
+
+class Square
+  include CandidateRemovers
+  attr_reader :spots
+
+  def initialize(spots)
+    @spots = spots
+  end
+end
+
+class Column
+  include CandidateRemovers
+  attr_reader :spots
+
+  def initialize(spots)
+    @spots = spots
+  end
+end
+
+class Row
+  include CandidateRemovers
+  attr_reader :spots
+  alias to_ary spots
+
+  def initialize(spots)
+    @spots = spots
   end
 end
--- lib/spot.rb
+++ lib/spot.rb
@@ -1,9 +1,10 @@
 class Spot
-  attr_reader :value, :candidates
+  attr_reader :candidates
+  attr_accessor :value
 
   def initialize(value)
     @value = value
-    @candidates = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    value == "-" ? @candidates = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] : @candidates = [value]
   end
 
   def remove_candidate(num)
